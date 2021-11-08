c38a017c0262fb5deb097b712f98070d1f20ebcd
YuchaoZheng88 <91292763+YuchaoZheng88@users.noreply.github.com>
2021-11-08 11:10:54 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import socket
 
-ServerIp=127.0.0.1
+ServerIp='127.0.0.1'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ServerIp, 4567))
+s.connect((ServerIp, 1234))
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
