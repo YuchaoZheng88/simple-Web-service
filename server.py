825b6e2b32449650a22c38e91dc69908b8947dbe
YuchaoZheng88 <91292763+YuchaoZheng88@users.noreply.github.com>
2021-11-08 10:50:47 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
+import socket
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 4567))
+
+s.listen(10)
+
+while True:
+  clientsocket, address = s.accept()
+  print(f"Connection from {address} has been established.")
+  clientsocket.send(bytes("Welcome","utf-8"))
+  
+  
+  
